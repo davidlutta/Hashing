19524367db6a8218960b891cc1166a8a643cb982
davidlutta <davidlutta2010@gmail.com>
2018-06-01 17:27:13 +0300

    Add some styling to the console

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ##Description
 This is a simple hashing project to learn how to hash passwords in Java using MD5 and the Java Security Library.
-
-`MessageDigest`, `Passwords`
+##How to use it
+1. Run `Gradle build`
+2. Navigate to `build/classes/java/main`
+3. Run `java App`
+4. Enter a test password and Your Hashed password will be displayed.
+##Tech Used
+`MessageDigest`, `MD5`
 
 `**THANK YOU**`
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,12 +5,15 @@ public class App{
     public static void main(String [] args){
         Console myConsole = System.console();
 
+        System.out.println("Enter your password please: ");
         String password = myConsole.readLine();
+            System.out.println("*******YOUR HASHED PASSWORD********");
         try {
             System.out.println(HashPassword.hashPassword(password));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+            System.out.println("******* Thank You ! *******");
 
     }
 }
